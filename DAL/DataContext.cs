8da91727902a6b0ff4e5a1cc0b24d21f5e029c82
OmarComish <omarthommy@gmail.com>
2024-11-07 12:27:59 -0800

    Added the dependencies for data connections

---
+++ DAL/DataContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ChurchPlus_v1._0.Models;
+using ChurchPlusAPI_v1._0.Models;
+
+
+namespace ChurchPlusAPI_v1.DAL;
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions options): base(options)
+    {
+          /*IConfigurationRoot configuration = new ConfigurationBuilder()
+          .SetBasePath(Environment.CurrentDirectory)
+          .AddJsonFile("appsettings.json")
+          .Build();*/
+          
+    }
+    public DbSet<AppLogs> AppLogs {get; set;}
+    public DbSet<CauseCategory> CauseCategories {get; set;}
+    public DbSet<Expense> Expenses {get; set;}
+    public DbSet<Offering> Offerings {get; set;}
+    public DbSet<OfferingGroup> OfferingGroups {get; set;}
+    public DbSet<Pledge> Pledges {get; set;}
+    public DbSet<Receipts> Receipts {get; set;}
+    public DbSet<RecordStatus> RecordStatuses {get; set;}
+    public DbSet<User> Users {get; set;}
+    public DbSet<UserRole> UserRoles {get; set;}
+}
