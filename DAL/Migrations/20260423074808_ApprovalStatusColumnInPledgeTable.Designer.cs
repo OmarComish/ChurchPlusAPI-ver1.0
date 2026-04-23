46c79a3a84f1c3514d7d5b54a265c3dba089d6e9
Comish Omar <Comish.Omar@nbs.mw>
2026-04-23 14:11:55 +0200

    Pledges endpoint done

--- DAL/Migrations/20260423074808_ApprovalStatusColumnInPledgeTable.Designer.cs
+++ DAL/Migrations/20260423074808_ApprovalStatusColumnInPledgeTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ChurchPlusAPI_v1.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ChurchPlusAPI_v1._0.DAL.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260423074808_ApprovalStatusColumnInPledgeTable")]
+    partial class ApprovalStatusColumnInPledgeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DAL/Migrations/20260423074808_ApprovalStatusColumnInPledgeTable.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChurchPlusAPI_v1._0.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApprovalStatusColumnInPledgeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Pledges",
+                newName: "PledgeStatus");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AmountPledged",
+                table: "Pledges",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ActualAmountFulfilled",
+                table: "Pledges",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ApprovalStatus",
+                table: "Pledges",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovalStatus",
+                table: "Pledges");
+
+            migrationBuilder.RenameColumn(
+                name: "PledgeStatus",
+                table: "Pledges",
+                newName: "Status");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AmountPledged",
+                table: "Pledges",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "ActualAmountFulfilled",
+                table: "Pledges",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
